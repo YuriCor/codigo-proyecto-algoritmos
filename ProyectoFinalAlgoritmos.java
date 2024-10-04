7b9faad35a2feadf0ed0d712926f1a5288751856
YuriCor <ycortezc2@miumg.edu.gt>
2024-10-03 21:08:44 -0600

    Add files via upload

--- ProyectoFinalAlgoritmos.java
+++ ProyectoFinalAlgoritmos.java
@@ -4,6 +4,11 @@
  */
 package proyectofinalalgoritmos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
@@ -32,25 +37,16 @@ public class ProyectoFinalAlgoritmos {
                 System.out.println(Opcion);
                 
                 switch (Opcion){
-                    case 1:
-                        gestionProductos();
-                        break;
-                        
-                    case 2:
-                        controlExistencias();
-                        break;
-                        
-                    case 3:
-                        pedidosCompras();
-                        break;
-                        
-                    case 4:
-                        informesEstadisticas();
-                        break;
-                        
+                    case 1: gestionProductos();
+                       break; 
+                    case 2: //controlExistencias();
+                        break; 
+                    case 3: //pedidosCompras();
+                        break; 
+                    case 4: //informesEstadisticas();
+                        break; 
                     case 5: // salida
-                                System.out.printl("CERRANDO PROGRAMA");
-                                break;
+                                System.out.println("CERRANDO PROGRAMA");
                                 
                 }
         } // cierra menu
@@ -70,16 +66,80 @@ public class ProyectoFinalAlgoritmos {
             Opcion= scan.nextInt();
             System.out.println(Opcion);
             switch (Opcion){
-                case 1:
-                    definicionCategorias();
+                case 1: definicionCategorias();
+                break;
+                case 2: definicionCaracteristicas();
+                break;
+                case 3: definicionEspecificaciones();
+                break;
+                case 4: definicionCaracEspecificacionesProductos();
+                break;
+                case 5: altaProductos();
+            }
+        } // cierre de la gestion de productos
+        static void definicionCategorias(){ // abre 
+            Scanner scan=new Scanner (System.in);
+            
+            int Opcion;
+            
+            System.out.println("CATEGORIA PRODUCTOS");
+            System.out.println("1. VER CATEGORIAS");
+            System.out.println("2. AGREGAR CATEGORIAS");
+            System.out.println("3. MODIFICAR CATEGORIAS");
+            System.out.println("4. ELIMINAR CATEGORIAS");
+            System.out.println("5. SALIR DE CATEGORIAS PRODUCTOS");
+            System.out.println("SELECCIONE LA OPCION NECESARIA");
+            Opcion= scan.nextInt();
+
+            switch (Opcion){
+                
+                case 1 -> mostrarCategorias();
+                case 2 -> agregarCategorias();
+                case 3 -> modificarCategorias();
+                case 4 -> eliminarCategorias();
+                case 5 -> {
+            }
+                default -> throw new AssertionError();
+            }
+        // para salir
+                } // Cierra Def Categorias
+        static void mostrarCategorias() throws FileNotFoundException, java.io.IOException { 
+            System.out.println("CATEGORIAS EXISTENTES");
+            try (BufferedReader br = new BufferedReader(new FileReader("categoria.txt"))){
+                String linea;
+                while ((linea = br.readLine())!= null) {
+                String[] partes = linea.split (" :V ");
+                    System.out.println("Nombre: " + partes[0]);
+                    System.out.println("Descripcion: " + (partes.length > 1 ? partes[1]: "NO EXISTE"));
+                    System.out.println();
+                }
             }
         }
-                                
+        static void agregarCategoria(){ // abre 
+            Scanner scan= new Scanner (System.in);
+            System.out.print("INGRESE LA CATEGORIA NUEVA: ");
+            String nombre = scan.nextLine();
+            
+            if (nombre.isEmpty()categoriaExiste(nombre)){
+            System.out.println("EL NOMBRE DE LA VATEGORIA NO PUEDE ESTAR VACIO O YA EXISTE. ");
+            return;
+        }
+            System.out.print();
+        }
+        }
+            
+                
+        
+                    
+                    
+            
+
+                         
                         
-                }
-                }
+                
+                
     
         
-    }
     
-}
+    
+
